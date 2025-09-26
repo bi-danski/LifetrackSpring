a41d2fdcc20ba0cf3c00fd3b9faf4ca8d74c1172
bi-danski <bidanski666@gmail.com>
2025-09-26 12:05:30 +0300

    ...

--- src/main/kotlin/org/lifetrack/lifetrackspring/utils/helpers.kt
+++ src/main/kotlin/org/lifetrack/lifetrackspring/utils/helpers.kt
@@ -1,9 +1,10 @@
 package org.lifetrack.lifetrackspring.utils
 
-import org.lifetrack.lifetrackspring.database.model.data.*
-import org.lifetrack.lifetrackspring.database.model.dto.BillingResponse
-import org.lifetrack.lifetrackspring.database.model.dto.UserDataRequest
-import org.lifetrack.lifetrackspring.database.model.dto.UserDataResponse
+import org.lifetrack.lifetrackspring.database.model.data.Billings
+import org.lifetrack.lifetrackspring.database.model.data.MedicalHistory
+import org.lifetrack.lifetrackspring.database.model.data.User
+import org.lifetrack.lifetrackspring.database.model.data.UserVitals
+import org.lifetrack.lifetrackspring.database.model.dto.*
 
 fun User.toRequest(): UserDataRequest {
     return UserDataRequest(
@@ -20,7 +21,7 @@ fun User.toResponse(): UserDataResponse {
     )
 }
 
-fun UserVitals.toVitalsResponse(): VitalsResponse{
+fun UserVitals.toVitalsResponse(): VitalsResponse {
     return VitalsResponse(this.pulse, this.bloodPressure, this.bodyTemperature, this.respiratoryRate, this.oxygenSaturation)
 }
 
@@ -28,6 +29,6 @@ fun Billings.toBillingsResponse(): BillingResponse {
     return BillingResponse(this.billingInfo)
 }
 
-fun MedicalHistory.toMedicalResponse(): MedicalResponse{
-    return MedicalResponse(this.allergies, this.chronicConditions, this.pastSurgeries, this.familyHistory, this.updatedAt, this.visits)
+fun MedicalHistory.toMedicalResponse(): MedicalResponse {
+    return MedicalResponse(this.allergies, this.chronicConditions, this.pastSurgeries, this.familyHistory, this.updatedAt)
 }
